b845d4b458b79abe725a4cc3418685092e93f177
YeonHo <kiteho8962@naver.com>
2021-07-22 01:36:29 +0900

    add attach.png

--- Campused/src/main/java/org/camp/used/file/controller/UploadController.java
+++ Campused/src/main/java/org/camp/used/file/controller/UploadController.java
@@ -5,13 +5,20 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+import org.camp.used.file.dto.AttachFileDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.log4j.Log4j;
@@ -76,49 +83,62 @@ public class UploadController {
 		log.info("=========upload ajax=========");
 	}
 	
-	@PostMapping("/uploadAjaxAction")
-	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+	@PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile) {
 		
 		log.info("===update ajax post===");
-		
+		List<AttachFileDTO> list = new ArrayList<>();
 		String uploadFolder = "C:\\campUpload\\temp";
 		
-		// make folder
-		File uploadPath = new File(uploadFolder, getFolder());
+		String uploadFolderPath = getFolder();
+		// make folder path
+		File uploadPath = new File(uploadFolder, uploadFolderPath);
 		log.info("upload path:" + uploadPath);
 		
 		if(uploadPath.exists() == false) {
 			uploadPath.mkdirs();
-		}
+		}// make folder
 		
 		// start loop
 		for(MultipartFile multipartFile : uploadFile) {
+			
+			AttachFileDTO attachFileDTO = new AttachFileDTO();
+			
+			String uploadFileName = multipartFile.getOriginalFilename();
+			
 			log.info("==================================");
 			log.info("Upload File Name:" +multipartFile.getOriginalFilename());
 			log.info("UploadFile Size: " +multipartFile.getSize());
-			
-			String uploadFileName = multipartFile.getOriginalFilename();
+			attachFileDTO.setFileName(uploadFileName);
+			log.info("===only file name:" + uploadFileName);
 			
 			UUID uuid = UUID.randomUUID();
 			uploadFileName = uuid.toString()+ "_" + uploadFileName;
 			
-			
-			
-			
 			try {
-				File saveFile = new File(uploadPath, uploadFileName); // File save
-				multipartFile.transferTo(saveFile);
+				File saveFile = new File(uploadPath, uploadFileName); 
+				multipartFile.transferTo(saveFile); // File save
+				
+				attachFileDTO.setUuid(uuid.toString());
+				attachFileDTO.setUploadPath(uploadFolderPath);
+				
 				// check image type
 				if(checkImageType(saveFile)) {
+					attachFileDTO.setImage(true);
 					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_"+uploadFileName));
 					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
 					thumbnail.close();
 				}
+				
+				list.add(attachFileDTO);
+				
 			} catch (IOException e) {
 				log.error(e.getMessage());
 				e.printStackTrace();
 			}
 		} // end loop
+		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
 	
 /////////////////////////////////////////////////////////////////////////////
